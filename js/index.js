7a3d4b3262e97b2014bbf8fa288b825092f9f5ce
Lucas F Guimaraes <lfguimara25@hotmai.com>
2024-06-13 12:01:14 -0300

    feat: Adicionando funcionalidade nos filtros
    Ao apertar na imagem do filtro, ele ira filtrar de acordo com o desejado

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     var filterButton = document.getElementById("filter");
-//     filterButton.addEventListener("click", function () {
-//         var productsContainer = document.getElementById("products");
-//         var products = Array.from(productsContainer.getElementsByClassName("product"));
+ document.addEventListener("DOMContentLoaded", function () {
+     var filterButton = document.getElementById("filter-a-z");
+     filterButton.addEventListener("click", function () {
+         var productsContainer = document.getElementById("products");
+         var products = Array.from(productsContainer.getElementsByClassName("product"));
 
-//         products.sort(function (a, b) {
-//             var nameA = a.querySelector(".product-name").innerText.toUpperCase();
-//             var nameB = b.querySelector(".product-name").innerText.toUpperCase();
-//             if (nameA < nameB) {
-//                 return -1;
-//             }
-//             if (nameA > nameB) {
-//                 return 1;
-//             }
-//             return 0;
-//         });
+         products.sort(function (a, b) {
+             var nameA = a.querySelector(".product-name").innerText.toUpperCase();
+             var nameB = b.querySelector(".product-name").innerText.toUpperCase();
+             if (nameA < nameB) {
+                 return -1;
+             }
+            if (nameA > nameB) {
+                 return 1;
+            }
+            return 0;
+        });
 
-//         products.forEach(function (product) {
-//             productsContainer.appendChild(product);
-//         });
-//     });
-// });
+        products.forEach(function (product) {
+           productsContainer.appendChild(product);
+        });
+   });
+ });
 
 
 
@@ -45,7 +45,7 @@ function sortProducts() {
 }
 
 // Evento de clique na imagem do filtro
-document.getElementById('filter').addEventListener('click', sortProducts);
+document.getElementById('filter-money').addEventListener('click', sortProducts);
 
 
 
