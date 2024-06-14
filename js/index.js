7d31b61299ab0ede6b2b01b1bc8b44fbffc2e2ea
Lucas F Guimaraes <lfguimara25@hotmai.com>
2024-06-14 08:29:46 -0300

    fix: Correção de bug no JS.
    Arrumando bug da seção 'filtro' no JS

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
- document.addEventListener("DOMContentLoaded", function () {
-     var filterButton = document.getElementById("filter-a-z");
-     filterButton.addEventListener("click", function () {
-         var productsContainer = document.getElementById("products");
-         var products = Array.from(productsContainer.getElementsByClassName("product"));
 
-         products.sort(function (a, b) {
-             var nameA = a.querySelector(".product-name").innerText.toUpperCase();
-             var nameB = b.querySelector(".product-name").innerText.toUpperCase();
-             if (nameA < nameB) {
-                 return -1;
-             }
+//Filter-left
+
+document.addEventListener("DOMContentLoaded", function () {
+    var filterButton = document.getElementById("filter-a-z");
+    filterButton.addEventListener("click", function () {
+        var productsContainer = document.getElementById("products");
+        var products = Array.from(productsContainer.getElementsByClassName("product"));
+
+        products.sort(function (a, b) {
+            var nameA = a.querySelector(".product-name").innerText.toUpperCase();
+            var nameB = b.querySelector(".product-name").innerText.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
             if (nameA > nameB) {
-                 return 1;
+                return 1;
             }
             return 0;
         });
 
         products.forEach(function (product) {
-           productsContainer.appendChild(product);
+            productsContainer.appendChild(product);
         });
-   });
- });
+    });
+});
 
 
 
-// Função para converter o preço de string para número
 function priceToNumber(price) {
-    return Number(price.replace(/[^0-9,-]+/g,"").replace(",", "."));
+    return Number(price.replace(/[^0-9,-]+/g, "").replace(",", "."));
 }
 
-// Função para ordenar os produtos
+
 function sortProducts() {
     let products = Array.from(document.querySelectorAll('.product'));
     products.sort((a, b) => {
@@ -44,11 +46,9 @@ function sortProducts() {
     });
 }
 
-// Evento de clique na imagem do filtro
 document.getElementById('filter-money').addEventListener('click', sortProducts);
 
-
-
+//Filter-right
 
 
 
